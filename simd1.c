0437995ec5a1edcebb2ad57c4067e2ef5a580745
Hannes Muehleisen <hannes@muehleisen.org>
2017-02-09 14:15:53 +0100

    somewhat optimized imprint generation

--- simd1.c
+++ simd1.c
@@ -1,6 +1,7 @@
 #include <ammintrin.h>
 #include <smmintrin.h>
 #include <immintrin.h>
+#include <sys/time.h>
 
 
 #include <stdio.h>
@@ -48,19 +49,35 @@ __m256i setbit_256(__m256i x,int k){
 }
 
 
-#define IMPRINT_BITS 256
 #define VALUES_PER_IMPRINT 256
 
 #define VALUE_BITS 32
 
+static struct timeval tm1;
+
+static inline void start()
+{
+    gettimeofday(&tm1, NULL);
+}
+
+static inline void stop()
+{
+    struct timeval tm2;
+    gettimeofday(&tm2, NULL);
+
+    unsigned long long t = 1000 * (tm2.tv_sec - tm1.tv_sec) + (tm2.tv_usec - tm1.tv_usec) / 1000;
+    printf("%llu\n", t);
+}
+
 
 int main(int argc,char** argv) {
-	int n = IMPRINT_BITS * 1000; // nicely divisible by 8 and 256
+	int imprint_bits = atoi(argv[2]);
+	int n = 256 * atoi(argv[1]); // nicely divisible by 8 and 256
 
 	int* values       = malloc(sizeof(int) * n);
 	char* value_ptr   = (char*) values;
-	int* limits       = malloc(sizeof(int) * IMPRINT_BITS);
-	__m256i* imprints = calloc(sizeof(__m256i) * n/IMPRINT_BITS, 1);
+	__m256i* limits       = malloc(sizeof(__m256i) * imprint_bits);
+	__m256i* imprints = calloc(sizeof(__m256i) * n/VALUES_PER_IMPRINT, 1);
 
 	if (!values || !limits || !imprints) {
 		return -1;
@@ -69,45 +86,79 @@ int main(int argc,char** argv) {
 	for (int i = 0; i < n; i++) {
 		values[i] = i; // best case for imprints, yes?
 	}
-	// some even-spaced limits
-	for (int i = 0; i < IMPRINT_BITS; i++) {
-		limits[i] = i* n/IMPRINT_BITS;
+	// some even-spaced limits, pre-set SIMD words
+	for (int i = 0; i < imprint_bits; i++) {
+		limits[i] = _mm256_set1_epi32(i* n/imprint_bits);
 	}
 
-	__m256i zero        = _mm256_setzero_si256();
-	__m256i one         = _mm256_set1_epi32(1);
+	__m256i zero = _mm256_setzero_si256();
 
 	__m256i bitmasks[256];
 	for (int i = 0; i < 256; i++) {
 		bitmasks[i] = setbit_256(zero, i);
 	}
 
+	start();
 	for (int chunk = 0; chunk < n/VALUES_PER_IMPRINT; chunk++) {
-
 		for (int chunk2 = 0; chunk2 < 32; chunk2++){
-
 			__m256i values_v    = _mm256_load_si256((__m256i*) value_ptr);
 			__m256i result      = _mm256_setzero_si256();
 
-			// TODO: Do this in larger groups
-			for (int l = 0; l < IMPRINT_BITS - 1; l++) {
-				__m256i limit1  = _mm256_set1_epi32(limits[l]);
-				__m256i result1 = _mm256_cmpgt_epi32(values_v, limit1);
-				// turn -1 from cmpgt into 1
-				result          = _mm256_add_epi32(result, result1);
+			// doing two limits at once was faster than a single one, but it made no difference between two and eight
+			// this is the critical part
+			for (int l1 = 0, l2=1; l1 < imprint_bits-2; l1+=2, l2+=2) {
+				result = _mm256_add_epi32(result, _mm256_add_epi32(_mm256_cmpgt_epi32(values_v, limits[l1]),_mm256_cmpgt_epi32(values_v, limits[l2])));
 			}
 			result = _mm256_abs_epi32(result);
 			
-			// todo: can we do better here going from indices to bit patterns?
-			for (int i = 0; i < IMPRINT_BITS/VALUE_BITS; i++) {
+			// in profiling, this was not performance-critical
+			for (int i = 0; i < imprint_bits/VALUE_BITS; i++) {
 				imprints[chunk] = _mm256_or_si256(imprints[chunk], bitmasks[_mm256_extract_epi32(result, i)]);
 			}
 			value_ptr += sizeof(__m256i);
-
 		}
-		printBits(sizeof(__m256i), &imprints[chunk]);
+	//	printBits(sizeof(__m256i), &imprints[chunk]);
+	}
+	stop();
+/*
+	int range_start = 60;
+	int  range_stop = 65;
+
+
+	__m256i query = zero;
+	__m256i query_inner = zero;
+
+	for (int i = range_start; i <= range_stop; i++) {
+		query = _mm256_or_si256(query, bitmasks[i]);
+	}
+	for (int i = range_start+1; i < range_stop; i++) {
+		query_inner = _mm256_or_si256(query_inner, bitmasks[i]);
 	}
 
+	__m256i query_inner_negation = zero;
+
+
+printBits(sizeof(__m256i), &query);
+printBits(sizeof(__m256i), &query_inner);
+
+
+	for (int i = 0; i < n/VALUES_PER_IMPRINT; i++) {
+		__m256i match = _mm256_and_si256(query, imprints[i]);
+		//__m256i match_inner = _mm256_and_si256(query_inner, imprints[i]);
+		_mm256_and_si256(query_inner, imprints[i]) // needs to have a 1
+		_mm256_andnot_si256(query_inner, imprints[i]) // needs to have no 1
+
+		// special case: inner match, all values are results
+		// normal case: check individual values
+
+
+		// todo: materialize result in memory (make a vector)
+
+		printBits(sizeof(__m256i), &match);
+	}
+*/
+//	
+//	printBits(sizeof(__m256i), &imprints[n/VALUES_PER_IMPRINT-1]);
 
 	return 0;
 }
