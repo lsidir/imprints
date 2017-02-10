8e2b6f7978d74db9dbd41e9f89f786f04b3d3115
Hannes Muehleisen <hannes@muehleisen.org>
2017-02-10 08:53:50 +0100

    added comparision with previous value and shaved another third of runtime off

--- simd1.c
+++ simd1.c
@@ -2,11 +2,7 @@
 #include <smmintrin.h>
 #include <immintrin.h>
 #include <sys/time.h>
-
-
 #include <stdio.h>
-
-char p[5] = {1, 1 << 1, 1 << 2, 1 << 3, 1 << 4};
 
 void dump(__m256i val) {
 		for (int i = 0; i < 8; i++) {
@@ -34,18 +30,15 @@ void printBits(size_t const size, void const * const ptr)
 }
 
 __m256i setbit_256(__m256i x,int k){
-// constants that will (hopefully) be hoisted out of a loop after inlining  
-  __m256i indices = _mm256_set_epi32(224,192,160,128,96,64,32,0);
-  __m256i one = _mm256_set1_epi32(-1);
-  one = _mm256_srli_epi32(one, 31);    // set1(0x1)
-
-
-  __m256i kvec = _mm256_set1_epi32(k);  
-// if 0<=k<=255 then kvec-indices has exactly one element with a value between 0 and 31
-  __m256i shiftcounts = _mm256_sub_epi32(kvec, indices);
-  __m256i kbit        = _mm256_sllv_epi32(one, shiftcounts);   // shift counts outside 0..31 shift the bit out of the element
-                                                               // kth bit set, all 255 other bits zero.
-  return _mm256_or_si256(kbit, x);                             // use _mm256_andnot_si256 to unset the k-th bit
+	// constants that will (hopefully) be hoisted out of a loop after inlining  
+	__m256i indices = _mm256_set_epi32(224,192,160,128,96,64,32,0);
+	__m256i one = _mm256_set1_epi32(-1);
+	one = _mm256_srli_epi32(one, 31);    // set1(0x1)
+	__m256i kvec = _mm256_set1_epi32(k);  
+	// if 0<=k<=255 then kvec-indices has exactly one element with a value between 0 and 31
+	__m256i shiftcounts = _mm256_sub_epi32(kvec, indices);
+	__m256i kbit        = _mm256_sllv_epi32(one, shiftcounts);   // shift counts outside 0..31 shift the bit out of the element
+	return _mm256_or_si256(kbit, x);                             // use _mm256_andnot_si256 to unset the k-th bit
 }
 
 
@@ -73,18 +66,21 @@ static inline void stop()
 int main(int argc,char** argv) {
 	int imprint_bits = atoi(argv[2]);
 	int n = 256 * atoi(argv[1]); // nicely divisible by 8 and 256
-
+	printf("Encoding %i values with %i bits\n", n, imprint_bits);
 	int* values       = malloc(sizeof(int) * n);
 	char* value_ptr   = (char*) values;
 	__m256i* limits       = malloc(sizeof(__m256i) * imprint_bits);
-	__m256i* imprints = calloc(sizeof(__m256i) * n/VALUES_PER_IMPRINT, 1);
 
-	if (!values || !limits || !imprints) {
+	// TODO: we should probably dynamically grow these?
+	__m256i* imprint_values = calloc(sizeof(__m256i) * n/VALUES_PER_IMPRINT, 1);
+	int*     imprint_counts = calloc(sizeof(int) * n/VALUES_PER_IMPRINT, 1);
+
+	if (!values || !limits || !imprint_values || !imprint_counts) {
 		return -1;
 	}
 	// generate some values
 	for (int i = 0; i < n; i++) {
-		values[i] = i; // best case for imprints, yes?
+		values[i] = 42; // best case for imprints, yes?
 	}
 	// some even-spaced limits, pre-set SIMD words
 	for (int i = 0; i < imprint_bits; i++) {
@@ -99,26 +95,50 @@ int main(int argc,char** argv) {
 	}
 
 	start();
-	for (int chunk = 0; chunk < n/VALUES_PER_IMPRINT; chunk++) {
+	int chunk = 0;
+	printf("n=%i, blocks=%i\n", n, n/VALUES_PER_IMPRINT);
+	while (value_ptr < ((char*) values) + sizeof(int) * n) {
+		__m256i imprintv = zero;
 		for (int chunk2 = 0; chunk2 < 32; chunk2++){
+				//			printf("val %i\n", *value_ptr);
+
 			__m256i values_v    = _mm256_load_si256((__m256i*) value_ptr);
-			__m256i result      = _mm256_setzero_si256();
+			__m256i result      = zero;
 
 			// doing two limits at once was faster than a single one, but it made no difference between two and eight
 			// this is the critical part
 			for (int l1 = 0, l2=1; l1 < imprint_bits-2; l1+=2, l2+=2) {
-				result = _mm256_add_epi32(result, _mm256_add_epi32(_mm256_cmpgt_epi32(values_v, limits[l1]),_mm256_cmpgt_epi32(values_v, limits[l2])));
+				result = _mm256_add_epi32(result, 
+					_mm256_add_epi32(
+						_mm256_cmpgt_epi32(values_v, limits[l1]),
+						_mm256_cmpgt_epi32(values_v, limits[l2])));
 			}
 			result = _mm256_abs_epi32(result);
+
+			//dump(result);
 			
-			// in profiling, this was not performance-critical
-			for (int i = 0; i < imprint_bits/VALUE_BITS; i++) {
-				imprints[chunk] = _mm256_or_si256(imprints[chunk], bitmasks[_mm256_extract_epi32(result, i)]);
+			// in profiling, this was not performance-critical, but doing two at once also gave a slight perf advantage
+			for (int i1 = 0, i2=1; i1 < imprint_bits/VALUE_BITS-1; i1+=2, i2+=2) {
+				imprintv = _mm256_or_si256(
+					_mm256_or_si256(imprintv, bitmasks[_mm256_extract_epi32(result, i1)]), 
+					_mm256_or_si256(imprintv, bitmasks[_mm256_extract_epi32(result, i2)]));
 			}
 			value_ptr += sizeof(__m256i);
 		}
-	//	printBits(sizeof(__m256i), &imprints[chunk]);
+		// _mm256_movemask_epi8 is a nice way of getting the result of a comparision into a normal int
+		int increment = chunk > 0 && _mm256_movemask_epi8(
+			_mm256_cmpeq_epi32(imprint_values[chunk], imprintv)) != -1;
+		chunk += increment;
+
+		imprint_values[chunk] = imprintv;
 	}
+	// TODO: we do not know at this point whether chunk points to the last or the n+1 element
+
+	// for (int ci = 0 ; ci <= chunk; ci++) {
+	// 	printBits(sizeof(__m256i), &imprint_values[ci]);
+
+	// }
+	printf("%i imprint(s)\n", chunk);
 	stop();
 /*
 	int range_start = 60;
