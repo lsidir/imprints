726b0c29cfd9c0ebbad610c7137a4971c648df67
Lefteris Sidirourgos <lsidir@gmail.com>
2018-02-12 16:46:23 +0100

    keeping cswxu's code for joins in joins branch

---
+++ joins/imps-join/src/generator.h
@@ -0,0 +1,60 @@
+/**
+ * @file    generator.h
+ * @author  Cagri Balkesen <cagri.balkesen@inf.ethz.ch>
+ * @date    Fri May 18 14:05:07 2012
+ * @version $Id: generator.h 3017 2012-12-07 10:56:20Z bcagri $
+ * 
+ * @brief  Provides methods to generate data sets of various types
+ * 
+ */
+
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include "types.h"
+#include "common.h"
+
+/** 
+ * @defgroup DataGeneration Data Set Generation
+ * @{
+ */
+
+/**
+ * Seed the random number generator before calling create_relation_xx. If not
+ * called, then generator will be initialized with the time of the call which
+ * produces different random numbers from run to run.
+ */
+void 
+seed_generator(unsigned int seed);
+
+/**
+ * Create relation with non-unique keys uniformly distributed between [0, maxid]
+ */
+int 
+create_column_nonunique(Column *col, const int32_t maxid);
+
+/**
+ * Create relation with only primary keys (i.e. keys are unique from 1 to
+ * col->colcount)
+ */
+int 
+create_relation_pk(Column* col);
+
+
+/** 
+ * Create a foreign-key column using the given primary-key relation.
+ * If the keys in pkcol is randomly distributed in
+ * the full integer range, then 
+ */
+int 
+create_column_fk_from_pk(Column *fkcol, Column *pkcol);
+
+/**
+ * Read column from file (one value per row as the format)
+ */
+int
+ReadColumnFromFile(Column *col, char * filename);
+
+/** @} */
+
+#endif /* GENERATOR_H */
---
+++ joins/imps-join/src/join.h
@@ -0,0 +1,45 @@
+/*
+ * join.h
+ *
+ *  Created on: 4 Jun 2017
+ *      Author: zeroxwj
+ */
+
+#ifndef JOIN_H_
+#define JOIN_H_
+
+#include "types.h"
+#include "common.h"
+#include "barrier.h"
+#include "hashtable.h"
+
+typedef struct {
+	unsigned int threadId; /* id of each thread */
+	unsigned int threadCount;
+	//ParallelJoin* join;
+	Column * colL;
+	Column * colR;
+	HashTable * ht;
+	pthread_barrier_t * barrier;	/* for thread sync */
+
+	unsigned int imps_bits;			/* # of bits used in hash value to encode imprints */
+
+	ThreadDctPointer* dctPointers;
+
+	uint64_t * matches;	/* to store the number of join result per thread */
+	unsigned int num_cacheline_per_thread;
+} JoinThreadArgs;
+
+
+#define PREFETCH_OFFSET 16
+#define MAX_IMPS_BITS 6
+
+
+/**
+ *
+ */
+query_result_t hashjoin(Column * colL, Column * colR, unsigned int threadCount, joinconf_t joinCfg);
+
+//query_result_t imps_hashjoin(Column * colL, Column * colR, unsigned int threadCount);
+
+#endif /* JOIN_H_ */
